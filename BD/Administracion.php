c5263551829c156413f3cfc62f9e1b6d55b542b2
Iago de Oliveira <bcniago@gmail.com>
2021-12-17 13:49:20 +0100

    arreglos aDMIN

--- BD/Administracion.php
+++ BD/Administracion.php
@@ -5,18 +5,39 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="../style/admin__style.css" />
+    <link rel="stylesheet" href="../style/nav__style.css" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Users Admins</title>
+    <title>Users_Admins</title>
 </head>
 
 <body>
-    <a href="./users.php">Users</a>
-    <?php if ($_SESSION['es_superadmin'] == 1) { ?>
-        <a href="./Admins.php">Admins</a>
-    <?php } ?>
-    <form action="./controlador.php" method="post">
-        <button type="submit" name="cerrarSesion">Cerrar sesion</button>
-    </form>
+    <div class="container-navbar">
+        <div class="logo-navbar">
+            <a href="../index.php"><img src="../media/img/marca-agua-negro.png" alt="" /></a>
+        </div>
+        <div class="contenido-navbar">
+            <div><a href="../index.php">Inicio</a></div>
+            <div><a href="../index.php#1">Sobre Politècnics</a></div>
+            <div><a href="../index.php#2">Ciclos</a></div>
+            <div><a href="../BD/loginUser.php">JUGAR</a></div>
+            <div>
+                <form action="./controlador.php" method="post">
+                    <div class="nombreUser-btn"><?php echo $_SESSION['email']; ?></div>
+                    <button class="cerrar-sesion" type="submit" name="cerrarSesion">Cerrar Sesión</button>
+                </form>
+            </div>
+        </div>
+    </div>
+    <div class="container-general">
+        <div class="container-user">
+            <div class="imagen"><a href="./users.php"><img src="../media/img/img-pantalla-game-2.png" /></a><?php if ($_SESSION['es_superadmin'] == 1) { ?></div>
+            <div class="titulo"><a>Users</a></div>
+        </div>
+        <div class="container-admin">
+            <div class="imagen"><a href="./Admins.php"><img src="../media/img/img-pantalla-game-2.png" /></a></div>
+            <div class="titulo"><a>Admins</a><?php } ?></div>
+        </div>
 </body>
 
 </html>
